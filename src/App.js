9fa219f92657644fa9f1ad7134a52114d8706f16
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-07-07 03:19:58 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import Potato from './Potato'
 
+// react magic -> props 처리
+// function Food(props)
+// props -> object -> 속성으로 전달한 값들
+function Food({fav}){
+  return <h3>I love {fav}!!!</h3>
+}
+
+// kimchi란 prop 전달
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
-      <Potato />
+      <Food fav="kimchi" /> 
+      <Food fav="ramen" /> 
+      <Food fav="gogi" /> 
     </div>
   );
 }
--- src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'; // 반드시 import 필요
-
-function Potato(){
-    return <h3>I love potato!</h3>
-}
-
-export default Potato; // export 필요
